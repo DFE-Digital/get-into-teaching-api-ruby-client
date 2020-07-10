3d33c1e48fb4389651750563ea77641254d61f38
maxmills <8balldigitalsolutions@gmail.com>
2020-07-10 13:29:22 +0100

    attempt 1

--- gem/lib/get_into_teaching_api_client/api/callback_booking_quotas_api.rb
+++ gem/lib/get_into_teaching_api_client/api/callback_booking_quotas_api.rb
@@ -1,7 +1,7 @@
 =begin
 #Get into Teaching API - V1
 
-#  Provides a RESTful API for integrating with the Get into Teaching CRM.    The Get into Teaching (GIT) API sits in front of the GIT CRM, which uses the [Microsoft Dynamics365](https://docs.microsoft.com/en-us/dynamics365/) platform (the [Customer Engagement](https://docs.microsoft.com/en-us/dynamics365/customerengagement/on-premises/developer/overview) module is used for storing Candidate information and the [Marketing](https://docs.microsoft.com/en-us/dynamics365/marketing/developer/using-events-api) module for managing Events).    The GIT API aims to provide:    * Simple, task-based RESTful APIs.  * Message queueing (while the GIT CRM is offline for updates).  * Validation to ensure consistency across services writing to the GIT CRM.                          
+#  Provides a RESTful API for integrating with the Get into Teaching CRM.    The Get into Teaching (GIT) API sits in front of the GIT CRM, which uses the [Microsoft Dynamics365](https://docs.microsoft.com/en-us/dynamics365/) platform (the [Customer Engagement](https://docs.microsoft.com/en-us/dynamics365/customerengagement/on-premises/developer/overview) module is used for storing Candidate information and the [Marketing](https://docs.microsoft.com/en-us/dynamics365/marketing/developer/using-events-api) module for managing Events).    The GIT API aims to provide:    * Simple, task-based RESTful APIs.  * Message queueing (while the GIT CRM is offline for updates).  * Validation to ensure consistency across services writing to the GIT CRM.
 
 OpenAPI spec version: v1
 
@@ -57,7 +57,7 @@ module GetIntoTeachingApiClient
         :form_params => form_params,
         :body => post_body,
         :auth_names => auth_names,
-        :return_type => 'CallbackBookingQuota')
+        :return_type => 'Array<CallbackBookingQuota>')
       if @api_client.config.debugging
         @api_client.config.logger.debug "API called: CallbackBookingQuotasApi#get_callback_booking_quotas\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
       end
